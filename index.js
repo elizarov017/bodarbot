ec10722adceb09e2032b96c9ca6471839555281c
elizarov017 <ftuti60@gmail.com>
2021-04-18 03:30:55 +0300

    df

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const bot = new TeleBot("1761355313:AAG-bKTHdZOwe5Vj54xTLbMdy5BWrcUYNg4");
 // const myModule = require('./m');
 // let val = myModule.ass1; // val is "Hello"   
 
-var getUserByID = (id) => { return users[id]; }; s
+var getUserByID = (id) => {
+  let i = users.findIndex((element, j) => {
+    return users[j]["id"] == id
+  });
+   return  users[i];
+}
+var updateDb = (user) => { 
+  users.push(user);
+  console.log(users);
+};
 
 // [${msg.from.first_name}](tg://user?id=${msg.from.id})
 
@@ -25,51 +34,74 @@ var getUserByID = (id) => { return users[id]; }; s
 // `
 
 
- 
+// TEXT
 
+const mainMenuText = "ГОЛОВНЕ МЕНЮ\n1. Розпочати пошук🔎\n2. Моя анкета📝";
 
 //  KEYBOARDS
 
-const mainMenu = [
-  [
-    {
-      text: '1',
-    }
-  ],
-  [
-    {
-      text: '2',
-    }
-  ],
-  [
-    {
-      text: '3',
-    }
-  ],
-  [
-    {
-      text: '4',
-    }
-  ]
-];
+let inSearch = (msgOld) => {
+   
+}
 
-const keyboard = [
-    [
-      {
-        text: 'Зареєструватись!', // текст на кнопке
+let myProfile = (msgOld) => {
+
+  let inMyProfile = true;
+  let user = getUserByID(msgOld.from.id);
+  // console.log(user);  
+  bot.sendPhoto(user["id"], user.photo, {
+    caption: `Ім'я: [${user["name"]}](tg://user?id=${user["id"]})\n\nВік: ${user["age"]} \n\nОпис: ${user["description"]}\n\nСтать: ${user["gender"] == "male"? "чоловіча":"жіноча"}\n\nШукаю: ${user["looking_for"] == "male"? "хлопця" :  user["looking_for"] == "both"? "без різниці" : "дівчину"}`
+    ,
+    parseMode: "Markdown"
+    ,
+    replyMarkup: {
+      keyboard: [ ["Перейти в головне меню"] ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    } 
+  });
+}
+              
+
+let mainMenu = (msgOld) => {
+  
+  let inMenu = true;
+  let userId = msgOld.from.id;
+  bot.sendMessage(userId, mainMenuText, {
+    replyMarkup: {
+      keyboard: [ ["1", "2"] ],
+      resize_keyboard: true,
+      one_time_keyboard: true
+    }
+  });
+  bot.on("*", (msg) => {
+    if (msg.from.id == userId && inMenu && msg.text != "Перейти в головне меню") {
+      if (msg.text == "1") {
+        inMenu = false;
+        inSearch(msg);
       }
-    ]
-    // ,
-    // [
-    //   {
-    //     text: 'Хочу песика',
-    //     callback_data: 'morePes'
-    //   }
-    // ]
-  ];
+      else if(msg.text == "2") {
+        inMenu = false;
+        myProfile(msg);
+      }
+      else {
+        bot.sendMessage(msg.from.id, "Вибери, будь ласка, один із варіантів.");
+        setTimeout(() => {
+          bot.sendMessage(msg.from.id, mainMenuText, {
+            replyMarkup: {
+              keyboard: [ ["1", "2"] ],
+              resize_keyboard: true,
+              one_time_keyboard: true
+            }
+          });
+        }, 500);
+      }
+    }
+  })
+};
 
 bot.on("/start", (msg) => {
-     // mode = 1;
+     
      bot.sendMessage(msg.from.id, `Привіт, ${msg.from.first_name}!\nЯ BodArBot і я допоможу тобі знайти другу половинку! Натискай кнопку і погнали!`, {
          replyMarkup: {
          keyboard: [
@@ -79,8 +111,8 @@ bot.on("/start", (msg) => {
             }
           ]
         ],
-         resize_keyboard: true,
-        //  one_time_keyboard: true
+        resize_keyboard: true,
+        one_time_keyboard: true
          }   
      });
 }); 
@@ -88,19 +120,50 @@ bot.on("/start", (msg) => {
 bot.on(/Зареєструватись!/, (msg) => {
   
   // bot.sendMessage(msg.from.id, `Чудово! Давай розпочнемо реєстрацію. Твоє ім'я?`);
-  
-  registration(msg);
-  // registration(msg);
-  
+  if (msg.from.username == undefined) {
+    bot.sendMessage(msg.from.id, "Вибач, але щоб зареєструватись в тебе має бути псевдонім, але його немає 😔\n[Ось інструкція як його створити](https://youtu.be/RtxySv4ANDU?t=23)", {
+      parseMode: "Markdown",
+      replyMarkup: {
+        keyboard: [
+          ["Все, псевдонім створив!"]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      }
+    })
+  }
+  else {
+    registration(msg);
+  }
 });
+
+bot.on([/Все, псевдонім створив!/, /Все, тепер точно!/], (msg) => {
+  if (msg.from.username == undefined) {
+    bot.sendMessage(msg.from.id, "Його все ще немає...", {
+      parseMode: "Markdown",
+      replyMarkup: {
+        keyboard: [
+          ["Все, тепер точно!"]
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      }
+    })
+  }
+  else {
+    registration(msg);
+  }
+});
+
 
 let registration = (msgOld) => {
 
   let step = 0;
   let userId = msgOld.from.id;
   let user = {
+    "id": "" + msgOld.from.id,
     "active": true,
-    "state": 4, // 0: notStarted | 1: main menu | 2: my info | 3: search | 4: registration
+    // 0: notStarted | 1: main menu | 2: my info | 3: search | 4: registration
     "name": "",
     "login": msgOld.from.username,
     "age": 0,
@@ -111,13 +174,8 @@ let registration = (msgOld) => {
     "history": {    }
   }
 
-  if (step == 0) {
-    
-  }
-
   bot.on("*", (msg) => {
-    // console.log(msg);
-    if (msg.from.id == userId) {
+    if (msg.from.id == userId && step < 8) {
       if (msg.text != undefined || step == 4 || step == 3) {
 
         switch (step){
@@ -276,17 +334,20 @@ let registration = (msgOld) => {
               if (msg.text == "Чоловіків") user["looking_for"] = "male";              
               else if(msg.text == "Дівчат") user["looking_for"] = "female";              
               else if (msg.text == "Без різниці") user["looking_for"] = "both";
-              user["state"] = 1;
-              bot.sendMessage(msg.from.id, "Все, реєстрацію закінчено. Ось твоя анкета:");
+              bot.sendMessage(msg.from.id, "Все, реєстрацію закінчено. Тепер можеш перейти в головне меню. Доречі, ось твоя анкета:");
               setTimeout(() => {
+                console.log(user.photo);
                 bot.sendPhoto(msg.from.id, user.photo, {
                   caption: `[${user.name}](tg://user?id=${msg.from.id})  -  ${user.age} \n\n${user.description}`,
                   parseMode: "Markdown",
                   replyMarkup: {
-                    remove_keyboard: true
+                    keyboard: [ ["Перейти в головне меню"] ],
+                    resize_keyboard: true,
+                    one_time_keyboard: true
                   }
                 });
               }, 1000);
+              updateDb(user);
               step++;
             }
             else {
@@ -302,6 +363,10 @@ let registration = (msgOld) => {
                 }
               });
             }
+            break;
+          case 7:
+            mainMenu(msg);
+            step++;
             break;
         }
       }
@@ -323,20 +388,22 @@ bot.on(/show*/, (msg) => {
 
 bot.start();
 
-var users= {
-    "id1": {
-        "active": true,
-        "state": 0, // 0: notStarted | 1: main menu | 2: my info | 3: search | 4: registration
-        "name": "",
-        "login": "",
-        "age": 0,
-        "gender": "",
-        "looking_for": "",
-        "description": "",
-        "photo": "",
-        "history": {
-            "id1": true,
-            "id2": false
-        }
-    }
-};
+var users= [
+    // { 
+    //   "id": "",
+    //   "active": true,
+    //    // 0: notStarted | 1: main menu | 2: my info | 3: search | 4: registration
+    //   "name": "",
+    //   "login": "",
+    //   "age": 0,
+    //   "gender": "",
+    //   "looking_for": "",
+    //   "description": "",
+    //   "photo": "",
+    //   "history": {
+    //       "id1": true,
+    //       "id2": false
+    //   }
+    // }
+];
+
